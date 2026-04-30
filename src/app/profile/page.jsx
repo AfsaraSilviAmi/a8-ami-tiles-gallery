3303c5765d1ba54022ba866dc18b8b12e0f69887
Afsara Silvi Ami <afsarasilvi44@gmail.com>
2026-04-30 22:52:51 +0600

    made update profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,9 @@
 'use client'
 import { authClient } from '@/lib/auth-client';
-import { Avatar, Card } from '@heroui/react';
+import { Avatar, Button, Card} from '@heroui/react';
+import Link from 'next/link';
 import React from 'react';
+import { BiEdit } from 'react-icons/bi';
 
 const ProfilePage = () => {
      const userData = authClient.useSession();
@@ -12,13 +14,17 @@ const ProfilePage = () => {
         <div className='my-10'>
            
                 <p className='font-bold text-3xl text-center text-pink-500 mb-5'>Welcome, <span className='text-black'>{user?.name}</span></p>
-                 <Card className='flex flex-col justify-center items-center h-[50vh] shadow-2xl w-[40%] mx-auto'>
+                 <Card className='flex flex-col justify-center items-center h-[60vh] shadow-2xl w-[40%] mx-auto'>
                 <Avatar className='w-40 h-40'>
                         <Avatar.Image alt={user?.name} src={user?.image} referrerPolicy='no-referrer' />
                         <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
                       </Avatar>
                       <p className='font-semibold text-2xl'>{user?.name}</p>
                       <p className='text-lg text-gray-600'>{user?.email}</p>
+                     
+     <Link href={"/update-profile"}> <Button className="bg-linear-to-r from-pink-500 via-purple-500 to-yellow-300 transition-all duration-300 hover:scale-110 hover:from-blue-600 hover:via-blue-400 hover:to-blue-200 hover:border-blue-500 text-lg border border-purple-400"><BiEdit></BiEdit>Edit Profile</Button></Link>
+     
+   
             </Card>
         </div>
     );
---
+++ src/app/update-profile/page.jsx
@@ -0,0 +1,46 @@
+'use client'
+import { authClient } from '@/lib/auth-client';
+import { Button, Form, Input, Label, TextField } from '@heroui/react';
+import React from 'react';
+
+const UpdateProfileForm = () => {
+    const onSubmit = async(e)=>{
+        e.preventDefault();
+
+         const name = e.target.name.value;
+         const image = e.target.image.value;
+
+         await authClient.updateUser(
+            {
+                name,
+                image
+            }
+         )
+
+    }
+    return (
+        
+              <div className='flex justify-center items-center h-[80vh]'>
+             <Form className="flex w-96 flex-col gap-4 bg-gray-100 p-6 rounded-lg shadow-xs" onSubmit={onSubmit}>
+                <h1 className='text-xl font-bold text-center'>Update Your Profile</h1>
+    <TextField className="w-full" name="name" type="text">
+                    <Label>Name</Label>
+                    <Input placeholder="Enter your name" />
+                  </TextField>
+                  <TextField className="w-full" name="image" type="url">
+                    <Label>Image URL</Label>
+                    <Input placeholder="Enter your image URL" />
+                  </TextField>
+      <div>
+      
+              <Button type="submit" className="w-full bg-linear-to-r from-pink-500 via-purple-500 to-yellow-300 transition-all duration-300 hover:scale-110 hover:from-blue-600 hover:via-blue-400 hover:to-blue-200 hover:border-blue-500">Update</Button>
+     
+      </div>
+      
+    </Form> 
+        </div>
+              
+    );
+};
+
+export default UpdateProfileForm;
